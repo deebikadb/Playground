73f6dbb5c4a227ee9fe1a076fc4a8cae684a1207
deebikadb <44392069+deebikadb@users.noreply.github.com>
2019-05-24 11:22:46 +0530

    Test code pushed to repository.

---
+++ Area square/Main.java
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  //Type your code here
+  int a, sqr;
+  scanf("%d", &a) ;
+  sqr=a*a;
+  printf("%d",sqr);
+  
+  return 0;
+}
---
+++ Product of two numbers/Main.java
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  //Type your code here
+  int a, b, pdt;
+  scanf("%d%d", &a, &b) ;
+  pdt=a*b;
+  printf("%d", pdt) ;
+  return 0;
+}
